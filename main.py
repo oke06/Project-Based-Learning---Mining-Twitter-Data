5edd011241a192ff174aae48b7de4f7cc170c2a3
oke06 <okatakyieba09@gmail.com>
2025-08-20 15:11:18 +0100

    2nd commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import tweepy
+from tweepy import OAuthHandler
+
+consumer_key = ''
+consumer_secret = ''
+access_token = '' 
+access_secret = ''
+
+auth = OAuthHandler(consumer_key, consumer_secret)
